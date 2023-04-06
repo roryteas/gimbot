35386b677905917ee1c3d36591380a378c5a79f8
Rory T <roryteasdale@gmail.com>
2023-04-06 20:53:47 +1200

    discord-app

--- simpledisc.js
+++ simpledisc.js
@@ -32,16 +32,3 @@ client.on("messageCreate", async (message) => {
 });
 
 client.login(token);
-
-// const express = require("express");
-// const app = express();
-// // port env or 3000
-// const port = process.env.PORT || 3000;
-
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
